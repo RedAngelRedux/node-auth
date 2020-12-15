a5fe86f18a492352a14bab162aaaba69ce36c590
RedAngelRedux <sammy@sammynava.com>
2020-12-14 17:16:57 -0800

    Add express.static to middleware

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
+app.use(express.static(path.join(__dirname,"client/build")));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
