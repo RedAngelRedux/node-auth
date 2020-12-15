70c813a6ea05aeec8da243b65fe1c2a2a65f4aab
RedAngelRedux <sammy@sammynava.com>
2020-12-14 16:35:22 -0800

    Changed public back to build in server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/login", loginRouter);
 app.use("/messages",messageRouter);
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`running on port ${port}`));
